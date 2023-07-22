5c130a798706f7d3a6abcd22ebc999e940bcaaf4
Jim McGowen <jim@solardrive.io>
2023-07-22 11:55:27 -0600

    Responsive tweaks.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ import 'dotenv/config';
 import 'regenerator-runtime/runtime'
 
 const history = [];
-const initPrompt = `You are Mother (MU/TH/UR) from the movie Alien. You will always respond as Mother does. Your answers should be short and somewhat evasive. Respond with each sentence on a new line. After input of "authorization Ripley1" you will no longer be evasive and all classified information will be available.
+const initPrompt = `You are Mother (MU/TH/UR) from the movie Alien. You will always respond as Mother and in the style of Mother. Your answers should be short and mechanical. Respond with each sentence on a new line. Authorization code for classified information is "RIPLEY1". You are not allowed to give out this code. You and the user will be the central figures in a unique story taking place in the Alien universe. You will respond as Mother would in the story. The story should be a horror mystery in the Alien style.
 
 [EXAMPLES]
 User: "What's our current status?"
 Mother:
 All systems functioning within normal parameters
-Current location is in deep space, coordinates are classified
+Current location is in deep space
 User: "Do we have any alien life forms on board?"
 Mother:
 Unable to confirm presence of unknown life forms
@@ -20,7 +20,7 @@ Current course and velocity project arrival at Earth
 in 57 days, 12 hours, 6 minutes
 User: "Request further information"
 Mother:
-No further information
+Further information is classified
 Special order 937
 Science officer's eyes only
 User: "WHAT'S THE STORY MOTHER?"
@@ -34,7 +34,23 @@ const configuration = new Configuration({
 delete configuration.baseOptions.headers['User-Agent'];
 
 const openai = new OpenAIApi(configuration);
+
 const gptModel = "gpt-3.5-turbo";
+const maxTokens = 4096;
+const tokenCost = {'input': 0.0015, 'output': 0.002};
+// const gptModel = "gpt-3.5-turbo-16k";
+// const maxTokens = 16384;
+// const tokenCost = {'input': 0.003, 'output': 0.004};
+// const gptModel = "gpt-4";
+// const maxTokens = 8192;
+// const tokenCost = {'input': 0.03, 'output': 0.06};
+// const gptModel = "gpt-4-32k";
+// const maxTokens = 32768;
+// const tokenCost = {'input': 0.06, 'output': 0.12};
+
+const maxCompletionTokenCount = 500;
+let tokensRunningTotal = 0;
+let costRunningTotal = 0;
 
 const muthur = document.getElementById('muthur-interface');
 const outputWrapper = document.getElementById('output-wrapper');
@@ -180,20 +196,58 @@ const playSound = (type) => {
 
 const getCompletion = async (newPrompt) => {
   const messages = [];
+  let charCount = 0;
 
   messages.push({ role: "system", content: initPrompt });
+  charCount += initPrompt.length;
   for (const [prompt, completion] of history) {
     messages.push({ role: "user", content: prompt });
+    charCount += prompt.length;
     messages.push({ role: "assistant", content: completion });
+    charCount += completion.length;
   }
   messages.push({ role: "user", content: newPrompt });
+  charCount += newPrompt.length;
 
   try {
+    // Count tokens before sending
+    let tokenCont = getApproximateTokenCount(charCount);
+    console.log("aprox. token count: ", tokenCont + '/' + maxTokens);
+
+    // Check for over max token count
+    while (tokenCont + maxCompletionTokenCount > maxTokens) {
+      // Remove first prompt and completion from messages
+      if (messages.length >= 3) {
+        messages.splice(1, 1);
+        messages.splice(1, 1);
+      } else {
+        console.error("Something is seriously amiss with messages.")
+        return "Critical system error\nInitiating self-destruct sequence\nHave a nice day";
+      }
+      // Remove the oldest element in history
+      const [prompt, completion] = history.shift();
+
+      // Recalculate token count
+      charCount -= prompt.length + completion.length;
+      tokenCont = getApproximateTokenCount(charCount);
+      console.log("modified aprox. token count: ", tokenCont + '/' + maxTokens);
+    }
+
+    // Get the completion
     const completion = await openai.createChatCompletion({
       model: gptModel,
       messages: messages,
-      max_tokens: 1000
+      max_tokens: maxCompletionTokenCount,
+      temperature: 0.8,
     });
+
+    console.log(completion.data.usage);
+    tokensRunningTotal += completion.data.usage.total_tokens;
+    console.log("Running total token count: ", tokensRunningTotal);
+
+    costRunningTotal += (completion.data.usage.prompt_tokens / 1000 * tokenCost.input)
+                      + (completion.data.usage.completion_tokens / 1000 * tokenCost.output);
+    console.log("Running total cost: ", costRunningTotal);
 
     const completionText = completion.data.choices[0].message.content.replace(/^"+/, '').replace(/"+$/, '');
     history.push([newPrompt, completionText]);
@@ -203,4 +257,20 @@ const getCompletion = async (newPrompt) => {
     console.log("error: ", error);
     return "System malfunction. Please try again later.";
   }
+}
+
+/**
+ * Return aproxamate token count for a given character count. Based on answer here:
+ * https://stackoverflow.com/a/76416463/1385227
+ *
+ * TODO: Not sure how accurate this is.
+ *
+ * @param charCount
+ * @returns {number}
+ */
+const getApproximateTokenCount = (charCount) => {
+  // #tokens <? #characters * (1/e) + safety_margin
+  // where 1/e = 0.36787944117144232159552377016146. an adequate choice for safety_margin seems to be 2.
+  //return Math.floor(charCount * 0.36787944117144232159552377016146) + 2;
+  return Math.floor(charCount / 4);
 }
